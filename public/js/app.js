4f29a7cbc11087be1c89eb157c484b2417fb2c4e
Bradley Pagano <bpagano5243@gmail.com>
2019-11-30 15:42:50 -0500

    got rid of the 'add to cart' buttons and added the 'on click' statement to the JS file

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+$("#complete").on("click", function() {
+  let gunCount = 0;
+  let bullCount = 0;
+  let parCount = 0;
+  let initDCount = 0;
+  let dartCount = 0;
+  let zeldaCount = 0;
+  let strageCount = 0;
+  let coffeeCount = 0;
+  let guacCount = 0;
+  let hamCount = 0;
+
+  parseInt($("#gun").val());
+  parseInt($("#bull").val());
+  parseInt($("#par").val());
+  parseInt($("#initD").val());
+  parseInt($("#darts").val());
+  parseInt($("#zelda").val());
+  parseInt($("#strange").val());
+  parseInt($("#coffee").val());
+  parseInt($("#guac").val());
+  parseInt($("#ham").val());
+});
