dba25715180d458ac805865d9a616ea19775ce7f
Bradley Pagano <bpagano5243@gmail.com>
2019-11-30 13:23:51 -0500

    adds server code

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("Server listening on PORT: " + PORT);
+  });
+});
